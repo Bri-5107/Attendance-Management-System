fade00b870c8c2421cefd750528f1a5f349c7c9b
Brian Dionisio <briandionisio@Arlenes-MacBook-Air.local>
2025-03-26 21:14:57 +0800

    chore: add initial maven setup

---
+++ src/main/java/io/github/brii/attendance_management_system/App.java
@@ -0,0 +1,10 @@
+package io.github.brii.attendance_management_system;
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
